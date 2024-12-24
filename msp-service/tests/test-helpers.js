06745d18490763bcbfd62f32f61c8a2e91921fdc
Skyler <skyler.mcnamee@hibc.gov.bc.ca>
2024-12-24 14:25:06 -0800

    add basic api tests

---
+++ msp-service/tests/test-helpers.js
@@ -0,0 +1,26 @@
+import * as fs from "fs";
+
+//to help ensure local servers are online before running integration tests
+export const tryServer = async (website, HTTPMethod) => {
+  const retryAttempts = 10;
+
+  for (let i = 0; i < retryAttempts; i++) {
+    try {
+      await fetch(website, {
+        method: HTTPMethod,
+      });
+      // console.log(`successfully reached ${website}!`);
+      return new Promise((resolve, reject) => {
+        resolve();
+      });
+    } catch (error) {
+      // console.log(`failed to reach ${website}, attempt `, i);
+      await new Promise((resolve) => setTimeout(resolve, 200));
+    }
+  }
+  return new Promise((resolve, reject) => {
+    reject(
+      `Couldn't reach ${website} (tried ${retryAttempts} times and gave up)`
+    );
+  });
+};
