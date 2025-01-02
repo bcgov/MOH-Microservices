538bd0812e03670f06efbb7102a08ed6da5381a4
Skyler <skyler.mcnamee@hibc.gov.bc.ca>
2025-01-02 12:28:28 -0800

    refactor generate port number script

--- msp-service/tests/test-helpers.js
+++ msp-service/tests/test-helpers.js
@@ -48,9 +48,28 @@ export const generateServiceCommand = (override) => {
   // return `LOG_LEVEL=${options.LOG_LEVEL} PORT=${options.PORT} TARGET_URL=${options.TARGET_URL} TARGET_USERNAME_PASSWORD=${options.TARGET_USERNAME_PASSWORD} MUTUAL_TLS_PEM_KEY_BASE64=${options.MUTUAL_TLS_PEM_KEY_BASE64} MUTUAL_TLS_PEM_KEY_PASSPHRASE=${options.MUTUAL_TLS_PEM_KEY_PASSPHRASE} MUTUAL_TLS_PEM_CERT=${options.MUTUAL_TLS_PEM_CERT} SECURE_MODE=${options.SECURE_MODE} USE_MUTUAL_TLS=${options.USE_MUTUAL_TLS} AUTH_TOKEN_KEY=${options.AUTH_TOKEN_KEY} USE_AUTH_TOKEN=${options.USE_AUTH_TOKEN} LOGGER_HOST=${options.LOGGER_HOST} LOGGER_PORT=${options.LOGGER_PORT} SPLUNK_AUTH_TOKEN=${options.SPLUNK_AUTH_TOKEN} timeout ${options.timeout} node src/index.js server`;
 };
 
+//List of used ports to decrease test flakiness
+let usedPorts = [];
+
 export const generatePortNumber = () => {
   //IANA officially recommends 49152-65535 for ephemeral ports
   const min = Math.ceil(49152);
   const max = Math.floor(65535);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+
+  const numberOfRetries = 10;
+
+  for (let i = 0; i < numberOfRetries; i++) {
+    const provisionalPort = Math.floor(Math.random() * (max - min + 1)) + min;
+
+    if (usedPorts.includes(provisionalPort)) {
+      console.log(`port number ${provisionalPort} already in use, regenerating...`)
+    } else {
+      usedPorts.push(provisionalPort);
+      return provisionalPort;
+    }
+  }
+  
+  throw new Error(
+    "Couldn't generate a unique port number (tried 10 times and gave up)"
+  );
 };
