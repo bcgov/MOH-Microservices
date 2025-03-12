8883234f788c59448609655dc2fc68f5a5d22f71
Skyler <skyler.mcnamee@hibc.gov.bc.ca>
2025-03-12 15:54:16 -0700

    refactor node husky stuff to make installing easier

--- .husky/install.mjs
+++ .husky/install.mjs
@@ -2,5 +2,14 @@
 if (process.env.NODE_ENV === "production" || process.env.CI === "true") {
   process.exit(0);
 }
-const husky = (await import("husky")).default;
-console.log(husky());
+
+try {
+  const husky = (await import("husky")).default;
+  console.log(husky());
+} catch (error) {
+  console.log(
+    "Husky install failed. Try running `npm i` at the project root first"
+  );
+  console.error("More information:", error);
+  process.exit(1);
+}
