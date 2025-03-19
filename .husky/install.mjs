604bb1c7869870d52dacaf1f8bfdd8ca4e420258
Skyler <skyler.mcnamee@hibc.gov.bc.ca>
2025-03-19 11:46:11 -0700

    refactor husky scripts

--- .husky/install.mjs
+++ .husky/install.mjs
@@ -1,17 +1,16 @@
 // Skip Husky install in production and CI
-console.log("(install.mjs) reached in the project root. checking environment")
 if (process.env.NODE_ENV === "production" || process.env.CI === "true") {
   process.exit(0);
 }
 
-console.log("(install.mjs) environment verified. preparing to execute husky install command")
 try {
   const husky = (await import("husky")).default;
   console.log(husky());
-  console.log("(install.mjs) husky successfully installed")
 } catch (error) {
-  console.log(
-    "Husky install failed. Try running `npm i` at the project root first"
+  console.error(
+    "\n",
+    `[ERROR] Husky failed. Run 'npm i' in the Microservices project root to install Git hook dependencies`,
+    "\n"
   );
   console.error("More information:", error);
   process.exit(1);
